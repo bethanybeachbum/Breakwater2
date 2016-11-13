f1a8d9978c36e553575d82cd3217eb1c511e5542
Mark Brown <allenmarkbrown@gmail.com>
2016-11-13 18:24:42 -0500

    Add files via upload
    Host-Ed Dowload

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+This is an AngularJS Command.
+Using the Angular framework, create a module (where we write pieces of angularjs code)
+'store' is the name of the application
+define dependencies in the array.
+Modules are where we write angular snippits of code
+*/
+var app = angular.module('store' []);
